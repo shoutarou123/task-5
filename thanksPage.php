1170d39501dd2c33c991373eae61a03d4440695b
shotaro <850123.a@gmail.com>
2025-08-10 15:06:59 +0900

    サンクスページ完成

--- thanksPage.php
+++ thanksPage.php
@@ -8,20 +8,23 @@ unset($_SESSION['form']);
 
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>checkPage</title>
+  <title>thanksPage</title>
   <link rel="stylesheet" href="/css/style.css" />
+  <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Protest+Strike&display=swap" rel="stylesheet">
 </head>
 
 <body>
   <?php include 'header.php'; ?>
   <main>
-
-    <p>ご予約ありがとうございました</p>
-
+    <div class="thanks_content">
+      <p>ご予約ありがとうございました</p>
+    </div>
   </main>
   <?php include 'footer.php'; ?>
 </body>
