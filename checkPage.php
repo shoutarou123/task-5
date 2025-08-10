35b5cb826fadc4dddd8da4808132ca94d2532b34
shotaro <850123.a@gmail.com>
2025-08-10 15:06:37 +0900

    確認ページ完成

--- checkPage.php
+++ checkPage.php
@@ -1,6 +1,7 @@
     <?php session_start();
     $_SESSION['form'] = $_POST;
     ?>
+
     <!DOCTYPE html>
     <html lang="ja">
 
@@ -9,57 +10,55 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>checkPage</title>
       <link rel="stylesheet" href="/css/style.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Protest+Strike&display=swap" rel="stylesheet">
     </head>
 
     <body>
       <?php include 'header.php'; ?>
       <main>
         <div class="check_content">
-          <p>席予約確認</p>
-          <form method="POST" action="index.php">
-            <div>
-              <label>お名前</label>
-              <span>
+          <p class="check_title">席予約確認</p>
+          <div class="flex">
+            <div class="gap">
+              <label class="check_label">お名前</label>
+              <label class="check_label">希望席</label>
+              <label class="check_label">メールアドレス</label>
+              <label class="check_label">電話番号</label>
+            </div>
+            <form class="gap" method="POST" action="index.php#subscription_title">
+              <div class="input_detail">
                 <?php echo htmlspecialchars($_SESSION['form']['name'] ?? '', ENT_QUOTES, 'UTF-8'); ?>
-              </span>
-              <input type="hidden" name="name" value="<?php echo htmlspecialchars($_SESSION['form']['name'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
-
-            </div>
-            <div>
-              <label>希望席</label>
-              <span>
+                <input type="hidden" name="name" value="<?php echo htmlspecialchars($_SESSION['form']['name'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+              </div>
+              <div class="input_detail">
                 <?php echo htmlspecialchars($_SESSION['form']['position'] ?? '', ENT_QUOTES, 'UTF-8'); ?>
-              </span>
-              <input type="hidden" name="position" value="
-              <?php echo htmlspecialchars($_SESSION['form']['position'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
-
-            </div>
-            <div>
-              <label>メールアドレス</label>
-              <span>
+                <input type="hidden" name="position" value="<?php echo htmlspecialchars($_SESSION['form']['position'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+              </div>
+              <div class="input_detail">
                 <?php echo htmlspecialchars($_SESSION['form']['email'] ?? '', ENT_QUOTES, 'UTF-8'); ?>
-              </span>
-              <input type="hidden" name="email" value="
-              <?php echo htmlspecialchars($_SESSION['form']['email'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
-
-            </div>
-            <div>
-              <label>電話番号</label>
-              <span>
+                <input type="hidden" name="email" value="<?php echo htmlspecialchars($_SESSION['form']['email'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+              </div>
+              <div class="input_detail">
                 <?php echo htmlspecialchars($_SESSION['form']['tel'] ?? '', ENT_QUOTES, 'UTF-8'); ?>
-              </span>
-              <input type="hidden" name="tel" value="
-              <?php echo htmlspecialchars($_SESSION['form']['tel'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+                <input type="hidden" name="tel" value="<?php echo htmlspecialchars($_SESSION['form']['tel'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+              </div>
+          </div>
 
+          <div class="flex_button">
+            <div>
+              <button class="button back" type="submit" name="back">戻る</button>
             </div>
-            <button type="submit" name="back">戻る</button>
-          </form>
+            </form>
+            <div>
+              <form method="POST" action="thanksPage.php">
+                <button class="button submit" type="submit" name="submit">送信</button>
+              </form>
+            </div>
+          </div>
+
         </div>
-
-
-        <form method="POST" action="thanksPage.php">
-          <button type="submit" name="submit">送信</button>
-        </form>
       </main>
       <?php include 'footer.php'; ?>
     </body>
